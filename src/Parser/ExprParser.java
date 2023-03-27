0eeb5197412f505aa4cc921fa2755bf7697bf403
dashluu <dashluu9121997@gmail.com>
2023-03-26 23:29:55 -0700

    Prepared for parsing expressions.

---
+++ src/Parser/ExprParser.java
@@ -0,0 +1,15 @@
+package Parser;
+
+public class ExprParser {
+
+    /**
+     * Grammar:
+     * Expr ::= Expr [binary operator] Expr
+     *          +/- Expr
+     *          ( Expr )
+     *          Term
+     * Term ::= var | number
+     */
+
+
+}
