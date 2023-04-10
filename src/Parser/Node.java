dd02e37b14fd4cf906aee9f5eeab3ede255ca9ce
dashluu <dashluu9121997@gmail.com>
2023-04-09 21:41:11 -0700

    Added support for iterator pattern, a method for counting the number of children, and overrode the equals() method.

--- src/Parser/Node.java
+++ src/Parser/Node.java
@@ -3,10 +3,36 @@ package Parser;
 import Tokens.Token;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Node {
+public class Node implements Iterable<Node> {
     private final Token token;
     private final ArrayList<Node> children = new ArrayList<>();
+
+    @Override
+    public Iterator<Node> iterator() {
+        return new NodeIter();
+    }
+
+    private class NodeIter implements Iterator<Node> {
+        private int index;
+
+        public NodeIter() {
+            index = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index < children.size();
+        }
+
+        @Override
+        public Node next() {
+            Node child = children.get(index);
+            ++index;
+            return child;
+        }
+    }
 
     public Node(Token token) {
         this.token = token;
@@ -22,5 +48,20 @@ public class Node {
 
     public void addChild(Node child) {
         children.add(child);
+    }
+
+    public int countChildren() {
+        return children.size();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Node node)) {
+            return false;
+        }
+        return token.equals(node.token);
     }
 }
