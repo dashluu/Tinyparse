6961e75ab039de3a2ed180714c131c7febf9f483
dashluu <dashluu9121997@gmail.com>
2023-04-09 21:42:16 -0700

    Removed all properties from OperatorInfo and used tables to store each operator's properties instead.

--- src/Reserved/OperatorInfo.java
+++ src/Reserved/OperatorInfo.java
@@ -3,28 +3,7 @@ package Reserved;
 import Tokens.TokenType;
 
 public class OperatorInfo extends ReservedInfo {
-    // Precedence
-    private final int preced;
-    private final boolean leftToRight;
-    // Operator type: NONE, UNARY, BINARY
-    private final OperatorType opType;
-
-    public OperatorInfo(TokenType id, int preced, boolean leftToRight, OperatorType opType) {
+    public OperatorInfo(TokenType id) {
         super(id, ReservedType.OPERATOR);
-        this.preced = preced;
-        this.leftToRight = leftToRight;
-        this.opType = opType;
-    }
-
-    public int getPreced() {
-        return preced;
-    }
-
-    public boolean isLeftToRight() {
-        return leftToRight;
-    }
-
-    public OperatorType getOperatorType() {
-        return opType;
     }
 }
