1a19b46143ebbe8b76f0ad06459974bab8be668c
dashluu <dashluu9121997@gmail.com>
2023-04-30 22:29:52 -0700

    Removed the testcase file for expression parser and added testcases directly to the test file.

--- src/Parser/ExprParserTest.java
+++ src/Parser/ExprParserTest.java
@@ -2,10 +2,10 @@ package Parser;
 
 import Exceptions.SyntaxError;
 import Lexer.Lexer;
+import Nodes.Node;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
 
@@ -13,8 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class ExprParserTest {
-    private Node parseExpr(String inputStr) throws SyntaxError, IOException {
-        BufferedReader reader = new BufferedReader(new StringReader(inputStr));
+    private Node parseExpr(String input) throws SyntaxError, IOException {
+        BufferedReader reader = new BufferedReader(new StringReader(input));
         Lexer lexer = new Lexer(reader);
         ExprParser exprParser = new ExprParser(lexer);
         Block scope = new Block(null);
@@ -26,7 +26,7 @@ class ExprParserTest {
             String value = root.getTok().getValue();
             switch (root.getType()) {
                 case TERMINAL -> {
-                    return value;
+                    return "(" + value + ")";
                 }
                 case UNARY -> {
                     String operand = recurTraverseAST(root.getChild(0));
@@ -44,13 +44,27 @@ class ExprParserTest {
 
     @Test
     void testValidExpr() {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("src/Parser/ExprParserTestCases.txt"));
-            String input, expected, actual;
-            Node root;
+        String[] inputArr = {
+                "1*2+3;",
+                "-1 + 2 * 3;",
+                "--12.* 326+ 4.3e1 / 97.0;",
+                "1*(2+3);"
+        };
 
-            while ((input = reader.readLine()) != null) {
-                expected = reader.readLine();
+        String[] expectedArr = {
+                "(((1)*(2))+(3))",
+                "((-(1))+((2)*(3)))",
+                "(((-(-(12.0)))*(326))+((4.3e1)/(97.0)))",
+                "((1)*((2)+(3)))"
+        };
+
+        String input, expected, actual;
+        Node root;
+
+        try {
+            for (int i = 0; i < inputArr.length; ++i) {
+                input = inputArr[i];
+                expected = expectedArr[i];
                 root = parseExpr(input);
                 actual = recurTraverseAST(root);
                 assertEquals(expected, actual);
