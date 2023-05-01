c8c64ccc6652caf5a0a9d5f3a888eef1ca11c999
dashluu <dashluu9121997@gmail.com>
2023-04-30 22:13:30 -0700

    Created DataTypeNode to store a node's data type.

---
+++ src/Nodes/DataTypeNode.java
@@ -0,0 +1,32 @@
+package Nodes;
+
+import Tokens.Token;
+import Types.TypeInfo;
+
+public class DataTypeNode extends Node {
+    protected TypeInfo dataType;
+
+    public DataTypeNode(Token tok, NodeType type, TypeInfo dataType) {
+        super(tok, type);
+        this.dataType = dataType;
+    }
+
+    public DataTypeNode(Token tok, NodeType type) {
+        this(tok, type, null);
+    }
+
+    public TypeInfo getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(TypeInfo dataType) {
+        this.dataType = dataType;
+    }
+
+    @Override
+    public String toJson() {
+        return super.toJson() +
+                ",\n\"Data type\": \"" + dataType.getId() +
+                "\",\n\"Size\": " + dataType.getSize();
+    }
+}
