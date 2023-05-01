25e0f60d6e6465ea356de1a25a455d97a73807d8
dashluu <dashluu9121997@gmail.com>
2023-04-30 22:37:32 -0700

    Added documentation and complete the initial implementation of declaration parser.

--- src/Parser/DeclParser.java
+++ src/Parser/DeclParser.java
@@ -2,8 +2,15 @@ package Parser;
 
 import Exceptions.SyntaxError;
 import Lexer.Lexer;
+import Nodes.DataTypeNode;
+import Nodes.Node;
+import Nodes.NodeType;
+import Nodes.VarNode;
+import Symbols.SymbolTable;
+import Symbols.VarInfo;
 import Tokens.Token;
 import Tokens.TokenType;
+import Types.TypeInfo;
 
 import java.io.IOException;
 
@@ -15,31 +22,73 @@ public class DeclParser extends BaseParser {
         this.exprParser = exprParser;
     }
 
-    public Node parseVarDecl(Block scope) throws SyntaxError, IOException {
-        // Try parsing the head
-        if (parseTok(TokenType.VAR_DECL) == null) {
-            return null;
-        }
-        // Check for variable name
-        Token varNameToken = lexer.consume();
-        if (varNameToken.getType() != TokenType.ID) {
-            throw new SyntaxError("Expected a variable name", lexer.getCurrLine());
-        }
-        Node varNameNode = new Node(varNameToken, NodeType.TERMINAL);
-        Node lhs = varNameNode;
-        // Check for type annotation, OK if it isn't there
-        Node typeNode = parseType();
-        if (typeNode != null) {
-            // Create a new tree if there is a data type
-            typeNode.addChild(varNameNode);
-            lhs = typeNode;
-        }
-        // Check for rhs initialization
-        Node initializationNode = parseInitializer(lhs, scope);
-        return initializationNode == null ? lhs : initializationNode;
+    /**
+     * Parses a declaration statement in a scope.
+     *
+     * @param scope the current scope surrounding the declaration.
+     * @return an AST node as the root if successful, otherwise, return null.
+     * @throws SyntaxError if there is a syntax error.
+     * @throws IOException if there is an IO exception.
+     */
+    public Node parseDecl(Block scope) throws SyntaxError, IOException {
+        DataTypeNode lhs = parseLhs(scope);
+        return parseAssignment(lhs, scope);
     }
 
-    private Node parseType() throws SyntaxError, IOException {
+    /**
+     * Parses the left-hand side(lhs) of a declaration.
+     *
+     * @param scope the current scope surrounding the declaration.
+     * @return an AST node representing the lhs if successful, otherwise, return null.
+     * @throws SyntaxError if there is a syntax error.
+     * @throws IOException if there is an IO exception.
+     */
+    private DataTypeNode parseLhs(Block scope) throws SyntaxError, IOException {
+        boolean mutable;
+
+        // Try parsing the declaration head and determining if it is a variable or a constant
+        if (parseTok(TokenType.VAR_DECL) == null) {
+            if (parseTok(TokenType.CONST_DECL) == null) {
+                return null;
+            } else {
+                mutable = false;
+            }
+        } else {
+            mutable = true;
+        }
+
+        // Check for variable or constant name
+        Token idTok = lexer.consume();
+        if (idTok.getType() != TokenType.ID) {
+            throw new SyntaxError("Expected a variable name", lexer.getCurrLine());
+        }
+
+        // Check for type annotation, OK if it isn't there
+        TypeInfo dataType = parseType();
+
+        // Check if it is a new variable or constant
+        SymbolTable symbolTable = scope.getSymbolTable();
+        String id = idTok.getValue();
+        if (symbolTable.getSymbol(id) != null) {
+            // If the variable or constant has been declared, throw an exception
+            throw new SyntaxError("Cannot redeclare a variable or a constant", lexer.getCurrLine());
+        } else {
+            // Create a new variable or constant if it does not exist
+            VarInfo varInfo = new VarInfo(id, dataType, mutable);
+            symbolTable.register(varInfo);
+        }
+
+        return new VarNode(idTok, NodeType.DECL, dataType, mutable);
+    }
+
+    /**
+     * Parses the type annotation on the lhs of a declaration.
+     *
+     * @return a TypeInfo object associated with the lhs's data type.
+     * @throws SyntaxError if there is a syntax error.
+     * @throws IOException if there is an IO exception.
+     */
+    private TypeInfo parseType() throws SyntaxError, IOException {
         // Check for ':'
         if (parseTok(TokenType.COLON) == null) {
             return null;
@@ -49,14 +98,36 @@ public class DeclParser extends BaseParser {
         if (token.getType() != TokenType.TYPE_ID) {
             throw new SyntaxError("Expected a type after ':'", lexer.getCurrLine());
         }
-        return new Node(token, NodeType.TYPE);
+        // Get the data type from the table using type id
+        return typeTable.getType(token.getValue());
     }
 
-    private Node parseInitializer(Node lhs, Block scope) throws SyntaxError, IOException {
+    /**
+     * Parses assignment and the right-hand side(rhs) of a declaration.
+     *
+     * @param lhs   the AST node that is associated with the lhs and contains the variable or constant's data type.
+     * @param scope the current scope surrounding the declaration.
+     * @return an AST node as the root if successful, otherwise, return null.
+     * @throws SyntaxError if there is a syntax error.
+     * @throws IOException if there is an IO exception.
+     */
+    private Node parseAssignment(DataTypeNode lhs, Block scope) throws SyntaxError, IOException {
         // Check for '='
         Token assignmentToken = parseTok(TokenType.ASSIGNMENT);
         if (assignmentToken == null) {
-            return null;
+            if (lhs == null) {
+                // If there is no lhs, return immediately
+                return null;
+            } else if (lhs.getDataType() == null) {
+                // Check if the data type of lhs has been declared
+                // If not, throw an exception
+                throw new SyntaxError("Cannot determine the type of '" + lhs.getTok().getValue() + "'",
+                        lexer.getCurrLine());
+            } else if (parseTok(TokenType.SEMICOLON) == null) {
+                // Otherwise, check if ';' is present for the case 'var id:type;'
+                throw new SyntaxError("Missing ';'", lexer.getCurrLine());
+            }
+            return lhs;
         }
         // Parse an expression using expression parser
         Node exprNode = exprParser.parseExpr(scope);
@@ -64,7 +135,7 @@ public class DeclParser extends BaseParser {
             throw new SyntaxError("Expected a nonempty expression after '='", lexer.getCurrLine());
         }
         // Construct a new tree whose root is '='
-        Node assignmentNode = new Node(assignmentToken, NodeType.DECL_ASSIGNMENT);
+        Node assignmentNode = new Node(assignmentToken, NodeType.ASSIGNMENT);
         assignmentNode.addChild(lhs);
         assignmentNode.addChild(exprNode);
         return assignmentNode;
