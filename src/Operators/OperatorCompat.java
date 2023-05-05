5b43619f898665056160645f7020bc1eb81ff960
dashluu <dashluu9121997@gmail.com>
2023-05-05 15:27:19 -0700

    Created the base class to check operands' type compatibility for an operator.

---
+++ src/Operators/OperatorCompat.java
@@ -0,0 +1,32 @@
+package Operators;
+
+import Tokens.TokenType;
+
+public abstract class OperatorCompat {
+    protected final TokenType id;
+    protected final OperatorCompatType compatType;
+
+    protected OperatorCompat(TokenType id, OperatorCompatType compatType) {
+        this.id = id;
+        this.compatType = compatType;
+    }
+
+    public TokenType getId() {
+        return id;
+    }
+
+    public OperatorCompatType getCompatType() {
+        return compatType;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof OperatorCompat opDataTypeCompat)) {
+            return false;
+        }
+        return id == opDataTypeCompat.id && compatType == opDataTypeCompat.compatType;
+    }
+}
