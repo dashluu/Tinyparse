d79adeb8176242b2a2e1e4b4a88f1613746e1130
dashluu <dashluu9121997@gmail.com>
2023-05-05 22:10:24 -0700

    Removed any usage of node's children.

--- src/Nodes/Node.java
+++ src/Nodes/Node.java
@@ -5,35 +5,9 @@ import Tokens.Token;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Node implements Iterable<Node> {
+public class Node {
     protected final Token tok;
     protected final NodeType type;
-    protected final ArrayList<Node> children = new ArrayList<>();
-
-    @Override
-    public Iterator<Node> iterator() {
-        return new NodeIter();
-    }
-
-    private class NodeIter implements Iterator<Node> {
-        private int index;
-
-        public NodeIter() {
-            index = 0;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return index < children.size();
-        }
-
-        @Override
-        public Node next() {
-            Node child = children.get(index);
-            ++index;
-            return child;
-        }
-    }
 
     public Node(Token tok, NodeType type) {
         this.tok = tok;
@@ -46,18 +20,6 @@ public class Node implements Iterable<Node> {
 
     public NodeType getType() {
         return type;
-    }
-
-    public Node getChild(int i) {
-        return children.get(i);
-    }
-
-    public void addChild(Node child) {
-        children.add(child);
-    }
-
-    public int countChildren() {
-        return children.size();
     }
 
     public String toJson() {
